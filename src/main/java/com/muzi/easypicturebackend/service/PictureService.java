d855d2e9d90cda87349e0304a401c219cef706c4
MuziGeek <572428344@qq.com>
2024-12-26 23:25:50 +0800

    第八阶段-（4分享链接 5批量编辑）

--- src/main/java/com/muzi/easypicturebackend/service/PictureService.java
+++ src/main/java/com/muzi/easypicturebackend/service/PictureService.java
@@ -73,6 +73,20 @@ public interface PictureService extends IService<Picture> {
      * @param picture
      */
     void checkPictureAuth(User loginuser, Picture picture);
-
+    /**
+     * 根据颜色搜索图片
+     *
+     * @param spaceId
+     * @param picColor
+     * @param loginUser
+     * @return
+     */
     List<PictureVO> searchPictureByColor(Long spaceId, String picColor, User loginUser);
+
+    /**
+     * 批量编辑图片
+     * @param pictureEditByBatchRequest
+     * @param loginUser
+     */
+    void editPictureByBatch(PictureEditByBatchRequest pictureEditByBatchRequest, User loginUser);
 }
